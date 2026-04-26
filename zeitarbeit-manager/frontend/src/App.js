70ea9002a6b2ae21eba265c4dec67ddd42676533
pcramos169 <pedrocalvino21@gmail.com>
2026-04-26 22:33:02 +0200

    Initial React setup for Frontend.

---
+++ zeitarbeit-manager/frontend/src/App.js
@@ -0,0 +1,12 @@
+{/* Frontend setup with React */}
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Welcome to Zeitarbeit Manager</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
